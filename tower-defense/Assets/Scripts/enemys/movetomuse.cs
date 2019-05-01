bc6914c34768333bb6bcbbe041cf006ca89da9bc
joris kleijn <34738260+jorisklijen@users.noreply.github.com>
2019-05-01 23:16:40 +0200

    ehh werkt kinda

---
+++ tower-defense/Assets/Scripts/enemys/movetomuse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class movetomuse : MonoBehaviour
+{
+   // public Camera cam;
+    public NavMeshAgent agent;
+    public Vector3 target;
+
+    void Update()
+    {
+        agent.SetDestination(target);
+
+     //   if (Input.GetMouseButtonDown(0))
+     //   {
+     //       Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+     //       RaycastHit hit;
+     //
+     //       if (Physics.Raycast(ray, out hit))
+     //       {
+     //           agent.SetDestination(hit.point);
+     //       }
+     //   }
+    }
+}
