595273d789cf4294af8f6c3c7fb334820843ca01
jorisklijen <34738260+jorisklijen@users.noreply.github.com>
2019-05-09 09:41:23 +0200

    fix

---
+++ tower-defense/Assets/Scripts/enemys/Spawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Geschreven door Joris de Kleijn [2-5-2019 19:00] 
+/// </summary>
+
+public class Spawner : MonoBehaviour
+{
+    public Transform enemy;
+    [Space]
+    public Transform spawnPoint;
+    [Space]
+    public float tijdTusenWaves = 5f;
+    // De tijd voor dat de eerste wave komt
+    private float countdown = 6f;
+   // [Space]
+    private int waveIndex = 0; 
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = tijdTusenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+
+    }
+
+
+    /// <summary>
+    /// corotine zorgt dat er een nieuwe groep vijhanden aankomen maar ze wel allemaal na elkaar spawnen
+    /// </summary>
+    IEnumerator SpawnWave ()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.2f);//----------------------------------- hier staat de tijd voor de induvuliwwele spawns van de enemys NIET DE SPAWNS VAN WAVES 
+        }
+        
+    }
+
+    /// <summary>
+    /// spawnd de ennemy op de goede plaats en met de goede rotaatie
+    /// </summary>
+    void SpawnEnemy()
+    {
+        Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+    }
+}
---
+++ tower-defense/Assets/Scripts/enemys/movetomuse.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;//--------------------------------------------------- nodig voor navmesh scripting
+
+/// <summary>
+/// Geschreven door Joris de Kleijn [2-5-2019 12:00] 
+/// </summary>
+
+public class movetomuse : MonoBehaviour
+{
+    // public Camera cam;
+    public NavMeshAgent agent;
+    [Space]  //----------------------------------------------------------- in editor zo dat er een space tussen zit :)
+    public Transform target;
+
+    void Update()
+    {
+        agent.SetDestination(target.position);
+
+        //----------------------------------[ Bewoog de enemy naar de muis klik ]-----------------------------------
+        //                                                                                                         |
+        //   if (Input.GetMouseButtonDown(0))                                                                      |
+        //   {                                                                                                     |
+        //       Ray ray = cam.ScreenPointToRay(Input.mousePosition);                                              |
+        //       RaycastHit hit;                                                                                   |
+        //       if (Physics.Raycast(ray, out hit))                                                                |
+        //           agent.SetDestination(hit.point);                                                              |
+        //   }                                                                                                     |
+        //                                                                                                         |
+        //----------------------------------------------------------------------------------------------------------
+    }
+}
