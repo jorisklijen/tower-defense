c18f080a4b84fd0d5fb58263b73f07e7a6dbf1b9
st1705 <39689688+st1705@users.noreply.github.com>
2019-05-13 12:53:45 +0200

    :D
    outline shader/material + klein scriptje voor building

---
+++ tower-defense/Assets/NodeScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class NodeScript : MonoBehaviour
+{
+    public Color hoverColor;
+    public Color normalColor;
+
+    private Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.SetColor("_Color", hoverColor);
+        Debug.Log("Mouse Enter");
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.SetColor("_Color", normalColor);
+        Debug.Log("Mouse Exit");
+    }
+}
