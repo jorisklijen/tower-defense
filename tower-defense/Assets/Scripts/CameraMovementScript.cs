658bd05b6be5f54352c79b1bd86e1e91a9f35ed6
st1705 <39689688+st1705@users.noreply.github.com>
2019-05-20 14:16:44 +0200

    brug + Turret Schieten + camera movement

---
+++ tower-defense/Assets/Scripts/CameraMovementScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class CameraMovementScript : MonoBehaviour
+{
+    public float panSpeed = 30f;
+    public float panBorderThickness = 10f;
+    public float scrollSpeed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) /*|| Input.mousePosition.y >= Screen.height - panBorderThickness*/)
+        {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.S)/* || Input.mousePosition.y <= Screen.height - panBorderThickness*/)
+        {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.A)/* || Input.mousePosition.x <= Screen.width - panBorderThickness*/)
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.D)/* || Input.mousePosition.x >= Screen.width - panBorderThickness*/)
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            transform.Translate(Vector3.forward * scrollSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            transform.Translate(Vector3.back * scrollSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ tower-defense/Assets/Scripts/Turrets/TurretScript.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretScript : MonoBehaviour
+{
+    [Header("Unity setup fields")]
+
+    private Transform target;
+    public string enemyTag = "Enemy";
+    public Transform partToRotate;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    [Space]
+    [Header("Atributes")]
+
+    public float fireRate = 1f;
+    private float fireContDown = 0f;
+    public float range = 15f;
+    public float rotateSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach(GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target == null)
+        {
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * rotateSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if(fireContDown <= 0)
+        {
+            Shoot();
+            fireContDown = 1f / fireRate;
+        }
+
+        fireContDown -= Time.deltaTime;
+
+    }
+
+    void Shoot()
+    {
+        Debug.Log("Shoot!");
+        GameObject bulletGo = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        BulletScript bullet = bulletGo.GetComponent<BulletScript>();
+        if(bullet != null)
+        {
+            bullet.Seek(target);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
