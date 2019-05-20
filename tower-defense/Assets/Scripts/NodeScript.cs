62ec09054b9f7fa47a73cc4039502183d638c316
jorisklijen <34738260+jorisklijen@users.noreply.github.com>
2019-05-20 16:14:09 +0200

    water

---
+++ tower-defense/Assets/Scripts/NodeScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class NodeScript : MonoBehaviour
+{
+    public Material hoverColor;
+    public Material normalColor;
+    public Material occupiedColor;
+
+    public Vector3 offset;
+
+    public AudioSource turretPlacementSource;
+
+    private GameObject turret;
+
+    private Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material = normalColor;
+    }
+
+    private void OnMouseDown()
+    {
+        //build a turret
+        if(turret != null) Debug.Log("Can't build here");
+        GameObject turretToBuild = BuildManager.instance.GetBuildTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + offset, transform.rotation);
+        turretPlacementSource.Play();
+    }
+
+    private void OnMouseEnter()
+    {
+        if (turret != null)
+        {
+            rend.material = occupiedColor;
+            return;
+        }
+
+        rend.material = hoverColor;
+        //Debug.Log("Mouse Enter");
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material = normalColor;
+        //Debug.Log("Mouse Exit");
+    }
+}
