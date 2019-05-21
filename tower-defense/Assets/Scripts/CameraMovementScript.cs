f824715791e9fe7d13092324f1442be94dc02f2e
jorisklijen <34738260+jorisklijen@users.noreply.github.com>
2019-05-21 16:37:07 +0200

    e

--- tower-defense/Assets/Scripts/CameraMovementScript.cs
+++ tower-defense/Assets/Scripts/CameraMovementScript.cs
@@ -9,6 +9,7 @@ public class CameraMovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
@@ -16,7 +17,7 @@ public class CameraMovementScript : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
-        }
+        }*/
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
@@ -25,7 +26,7 @@ public class CameraMovementScript : MonoBehaviour
         {
             transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
         }
-
+        /*
         if(Input.GetAxis("Mouse ScrollWheel") > 0)
         {
             transform.Translate(Vector3.forward * scrollSpeed * Time.deltaTime);
@@ -34,5 +35,6 @@ public class CameraMovementScript : MonoBehaviour
         {
             transform.Translate(Vector3.back * scrollSpeed * Time.deltaTime);
         }
+        */
     }
 }
