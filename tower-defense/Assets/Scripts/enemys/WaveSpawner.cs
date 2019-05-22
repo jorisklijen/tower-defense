9734e538e05c07cd6531a45eea493a552a6a4720
jorisklijen <34738260+jorisklijen@users.noreply.github.com>
2019-05-22 09:17:34 +0200

    ASDFG

---
+++ tower-defense/Assets/Scripts/enemys/WaveSpawner.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+
+    public enum SpawnState { Spawning, Waitng, Counting}
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+
+    private SpawnState State = SpawnState.Counting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waveCountdown = timeBetweenWaves;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waveCountdown <= 0)
+        {
+            if (State != SpawnState.Spawning)
+            {
+                // spawn wave hier 
+            }
+        }
+        else
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+    }
+
+   /* IEnumerator SpawnWave(Wave _wave)
+    {
+        State = SpawnState.Spawning;
+
+        //spawn
+
+
+    }
+
+    */
+
+
+}
