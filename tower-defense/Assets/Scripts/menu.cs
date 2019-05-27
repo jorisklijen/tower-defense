ebe16e6381c215dd528e159fba8cb438c9d48fb4
joris kleijn <34738260+jorisklijen@users.noreply.github.com>
2019-05-27 10:01:24 +0200

    mernu

---
+++ tower-defense/Assets/Scripts/menu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public void PlayMainGame()
+    {
+        SceneManager.LoadScene("MAIN");
+    }
+
+    public void BackToMainGame()
+    {
+        SceneManager.LoadScene("menu");
+    }
+
+
+
+
+    public void QuitGame ()                                                                                                                                                                                 //hoi
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+
+
+
+
+
+
+
+}
